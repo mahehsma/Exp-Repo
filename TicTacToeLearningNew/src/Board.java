cc24d59aa6f8f6315f9c165f3d911eb9c930125e
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-02-03 19:41:17 +0100

    adding minimax

--- TicTacToeLearningNew/src/Board.java
+++ TicTacToeLearningNew/src/Board.java
@@ -1,54 +1,63 @@
 
 public class Board {
-	char field[] = new char[9];
+	private char field[] = new char[9];
 
 	Board() {
 		clearBoard();
 	}
-	
-	void setField(int pos, char xOrO){
-		if(field[pos] == ' ') {
+
+	void setField(int pos, char xOrO) {
+		if (field[pos] == ' ') {
 			field[pos] = xOrO;
 		} else {
-			System.out.println("Field "+pos+" is not empty! Choose another field!");
+			System.out.println("Field " + pos + " is not empty! Choose another field!");
 		}
 	}
-	
-	char[] getField(){
+
+	char[] getField() {
 		return field;
 	}
-	
+
 	void printField() {
 		System.out.println("");
-		for(int i=0;i<9;i++) {
-			System.out.print("["+field[i]+"] ");
-			if((i+1)%3==0) {
+		for (int i = 0; i < 9; i++) {
+			System.out.print("[" + field[i] + "] ");
+			if ((i + 1) % 3 == 0) {
 				System.out.println();
 			}
 		}
 	}
 
-	boolean hasWon() {
-		//checks rows
-		if ((field[0] == field[1] && field[1] == field[2] && field[0] != ' ')
-				|| (field[3] == field[4] && field[4] == field[5] && field[3] != ' ')
-				|| (field[6] == field[7] && field[7] == field[8] && field[6] != ' ')
-				
-				//checks columns
-				|| (field[0] == field[3] && field[3] == field[6] && field[0] != ' ')
-				|| (field[1] == field[4] && field[4] == field[7] && field[1] != ' ')
-				|| (field[2] == field[5] && field[5] == field[8] && field[2] != ' ')
-				
-				//checks diagonals
-				|| (field[0] == field[4] && field[4] == field[8] && field[0] != ' ')
-				|| (field[2] == field[4] && field[4] == field[6] && field[2] != ' ')) {
-			
+	boolean tie(char checkField[]) {
+		for (int i = 0; i < 9; i++) {
+			if (checkField[i] == ' ') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	boolean hasWon(char checkField[]) {
+		// checks rows
+		if ((checkField[0] == checkField[1] && checkField[1] == checkField[2] && checkField[0] != ' ')
+				|| (checkField[3] == checkField[4] && checkField[4] == checkField[5] && checkField[3] != ' ')
+				|| (checkField[6] == checkField[7] && checkField[7] == checkField[8] && checkField[6] != ' ')
+
+				// checks columns
+				|| (checkField[0] == checkField[3] && checkField[3] == checkField[6] && checkField[0] != ' ')
+				|| (checkField[1] == checkField[4] && checkField[4] == checkField[7] && checkField[1] != ' ')
+				|| (checkField[2] == checkField[5] && checkField[5] == checkField[8] && checkField[2] != ' ')
+
+				// checks diagonals
+				|| (checkField[0] == checkField[4] && checkField[4] == checkField[8] && checkField[0] != ' ')
+				|| (checkField[2] == checkField[4] && checkField[4] == checkField[6] && checkField[2] != ' ')) {
+
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	void clearBoard() {
 		for (int i = 0; i < 9; i++) {
 			field[i] = ' ';
--- TicTacToeLearningNew/src/TicTacToeMain.java
+++ TicTacToeLearningNew/src/TicTacToeMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 
 /**
- * @author maxwi
+ * @author mahehsma
  *
  */
 public class TicTacToeMain {
@@ -18,6 +18,7 @@ public class TicTacToeMain {
 		boolean playerIsNext = true;
 		System.out.println("Welcome to TicTacToe");
 		for (int i = 0; i < 9; i++) {
+			System.out.println("Round :" + i);
 			if (playerIsNext) {
 				// input user
 				System.out.println("Your turn! Choose an empty field!");
@@ -30,11 +31,12 @@ public class TicTacToeMain {
 				playerIsNext = false;
 			} else {
 				// computer
-				board.setField(chooseField(board.getField()), 'O');
+				// board.setField(chooseField(board.getField(), board), 'O');
+				board.setField(bestMove(board), 'O');
 				playerIsNext = true;
 			}
 			board.printField();
-			if (board.hasWon()) {
+			if (board.hasWon(board.getField())) {
 				if (playerIsNext) {
 					System.out.println("PC has won!");
 					i = 9;
@@ -46,11 +48,134 @@ public class TicTacToeMain {
 				}
 			}
 		}
+		if (!board.hasWon(board.getField())) {
+			System.out.println("Draw!");
+		}
 
 	}
-	public static int chooseField(char field[]) {
-		//logic
-		return 1;
+
+	public static int bestMove(Board board) {
+		int score;
+		int bestMove = 0;
+		int bestScore = -1;
+		char[] testField = new char[9];
+		for (int i = 0; i < 9; i++) {
+			testField[i] = board.getField()[i];
+		}
+		for (int i = 0; i < 9; i++) {
+			if (testField[i] == ' ') {
+				testField[i] = 'O';
+				score = minimax(board, false, testField);
+				testField[i] = ' ';
+				if (score > bestScore) {
+					bestScore = score;
+					bestMove = i;
+				}
+			}
+		}
+		return bestMove;
 	}
+
+	public static int minimax(Board board, boolean isMaximizing, char[] testField) {
+		if (board.hasWon(testField)) {
+			int score;
+			if (isMaximizing) {
+				score = 1;
+			} else {
+				score = -1;
+			}
+			return score;
+		}
+		if (board.tie(testField)) {
+			return 0;
+		}
+		if (isMaximizing) {
+			int score = -1;
+			int bestScore = -1;
+			for (int i = 0; i < 9; i++) {
+				if (testField[i] == ' ') {
+					testField[i] = 'O';
+					score = minimax(board, false, testField);
+					testField[i] = ' ';
+					if (score > bestScore) {
+						bestScore = score;
+					}
+				}
+			}
+			return bestScore;
+		} else {
+			int score = 1;
+			int bestScore = 1;
+			for (int i = 0; i < 9; i++) {
+				if (testField[i] == ' ') {
+					testField[i] = 'X';
+					score = minimax(board, true, testField);
+					testField[i] = ' ';
+					if (score < bestScore) {
+						bestScore = score;
+					}
+				}
+			}
+			return bestScore;
+		}
+	}
+
+//	public static int chooseField(char field[], Board board) {
+//		// logic
+//		boolean playerIsNext = false;
+//		double moveEvaluation[] = scanForBestMove(field, board, playerIsNext);
+//		int pointer = 0;
+//		for (int i = 0; i < 9; i++) {
+//			System.out.println(moveEvaluation[i]);
+//			if (moveEvaluation[pointer] < moveEvaluation[i]) {
+//				pointer = i;
+//			}
+//		}
+//		System.out.println("pointer: " + pointer);
+//		return pointer;
+//	}
+
+//	public static double[] minimax(char field, Board board, boolean playerIsNext) {
+//		char field2[] = new char[9];
+//
+//	}
+
+//	public static double[] scanForBestMove(char field[], Board board, boolean playerIsNext) {
+//		char testField[]=new char[9];
+//		double bestMove[] = new double[9];
+//		for(int k=0;k<9;k++) {
+//			bestMove[k]=0;
+//			//field is not empty
+//			
+//			testField[k]=field[k];
+//		}
+//		for (int i = 0; i < 9; i++) {
+//			if (testField[i] == ' ') {
+//				double help[];
+//				if (playerIsNext) {
+//					testField[i] = 'X';
+//					help=scanForBestMove(testField, board, false);
+//				} else {
+//					testField[i] = 'O';
+//					help=scanForBestMove(testField, board, true);
+//				}
+//				if (board.hasWon(testField)) {
+//					if (playerIsNext) {
+//						bestMove[i] = 1;
+//					} else {
+//						bestMove[i] = 1;
+//					}
+//
+//				} else {
+//					testField[i] = ' '; //resets the move
+//					for(int k=0;k<9;k++) {
+//						bestMove[i]=bestMove[i]+help[k]/9.0;
+//					}
+//				}
+//			}
+//
+//		}
+//		return bestMove;
+//	}
 
 }
